c0b45a52cab77c7a75cd3098378a1ed6e3b40442
Hummr01 <hummr@hummr.me>
2021-09-22 17:56:34 +0200

    setup

---
+++ blog-page/src/topbar/TopBar.tsx
@@ -0,0 +1,15 @@
+import "./topbar.css"
+
+export default function TopBar() {
+    return(
+
+        <div className="top">
+            <div className="logo">
+                <h1>Hummr</h1>
+            </div>
+        </div>
+
+    )
+
+
+}
